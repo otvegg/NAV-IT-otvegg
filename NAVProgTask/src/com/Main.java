3c3d3aa9174030eab2e8aa931b053da31027f563
otvegg <timian@husveg.net>
2021-02-05 03:22:27 +0100

    Project mostly done

--- NAVProgTask/src/com/Main.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class Main {
-
-    public static void main(String[] args) {
-        PaymentCalculator calculator = new PaymentCalculator();
-        calculator.calculate();
-    }
-}
